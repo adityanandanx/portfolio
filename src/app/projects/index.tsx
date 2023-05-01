a7a8a338f937f5964d10d59294088149f61ac5eb
iMADi-ARCH <nandanaditya985@gmail.com>
2023-05-01 17:48:52 +0530

    chore: add links

--- src/app/projects/index.tsx
+++ src/app/projects/index.tsx
@@ -15,8 +15,8 @@ const index: FC<indexProps> = ({}) => {
                 desc="A todo with minimal design and fast hotkeys for everything."
                 tags={["next", "react", "zustand", "tailwind", "radix"]}
                 imgPath={todolistImg}
-                github={"/"}
-                live={"/"}
+                github={"https://github.com/iMADi-ARCH/Todolist-zustand"}
+                live={"https://todolist-zustand.vercel.app/"}
             />
             <Project
                 className="bg-green-800 dark:bg-green-800 text-stone-50"
@@ -24,8 +24,8 @@ const index: FC<indexProps> = ({}) => {
                 desc="Simple minimalistic trivia which consumes the Open Trivia Database (opentdb)"
                 tags={["next", "react", "tailwind", "radix"]}
                 imgPath={triviaImg}
-                github={"/"}
-                live={"/"}
+                github={"https://github.com/iMADi-ARCH/Trivia"}
+                live={"https://trivia-bice.vercel.app/"}
             />
             <Project
                 className="bg-purple-950 dark:bg-purple-950 text-stone-50"
@@ -33,8 +33,8 @@ const index: FC<indexProps> = ({}) => {
                 desc="This web app displays daily, weekly, and monthly prices for a variety of digital currencies including Bitcoin and Ethereum."
                 tags={["next", "react", "zustand", "tailwind", "radix"]}
                 imgPath={cryptImg}
-                github={"/"}
-                live={"/"}
+                github={"https://github.com/iMADi-ARCH/cryptracker"}
+                live={"https://cryptracker-dun.vercel.app/"}
             />
         </section>
     );
