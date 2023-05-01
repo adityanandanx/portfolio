17e38d68f9eb524e7d6aff2f8bfed5026b484166
iMADi-ARCH <nandanaditya985@gmail.com>
2023-05-01 21:40:14 +0530

    feature: Add contact form

--- src/app/talk/index.tsx
+++ src/app/talk/index.tsx
@@ -1,5 +1,6 @@
 "use client";
 import Button from "@/components/ui/Button";
+import Field from "@/components/ui/Field";
 import Socials from "@/components/ui/Socials";
 import { Syne } from "next/font/google";
 import { FC } from "react";
@@ -11,9 +12,9 @@ interface TalkProps {}
 
 const Talk: FC<TalkProps> = ({}) => {
     return (
-        <section className="w-full h-[90vh] p-0">
-            <div className="w-full p-16 md:p-24 bg-stone-800 dark:bg-stone-700 text-stone-50 rounded-xl h-full flex flex-col items-start justify-between">
-                <div>
+        <section className="w-full min-h-[90vh] p-0">
+            <div className="w-full h-full p-16 md:p-24 bg-stone-800 dark:bg-stone-700 text-stone-50 rounded-xl flex flex-col items-start justify-between">
+                <div className="h-full">
                     <h2 className="text-2xl">Have a project for Me?</h2>
                     <h1
                         style={{ ...syne.style }}
@@ -21,10 +22,26 @@ const Talk: FC<TalkProps> = ({}) => {
                     >
                         Let&rsquo;s Talk
                     </h1>
-                    <Button className="text-base leading-none py-4 bg-stone-50 text-stone-900">
-                        Get in touch
-                        <MdChevronRight className="transform scale-150" />
-                    </Button>
+                    <form
+                        action="mailto:dev2005adi@gmail.com"
+                        method="get"
+                        encType="text/plain"
+                        className="max-w-md flex flex-col gap-3 h-full flex-1 my-20"
+                    >
+                        <Field required id="subject" labelText="Subject" />
+                        <Field required id="email" labelText="Your Email" />
+                        <Field
+                            required
+                            id="body"
+                            labelText="Message"
+                            placeholder="Your Message"
+                            type="textarea"
+                        />
+                        <Button className="text-base leading-none py-4 bg-stone-50 text-stone-900 self-start">
+                            Get in touch
+                            <MdChevronRight className="transform scale-150" />
+                        </Button>
+                    </form>
                 </div>
                 <footer className="w-full flex flex-col md:flex-row md:items-center justify-between">
                     <span style={syne.style} className="text-2xl">
---
+++ src/components/ui/Field.tsx
@@ -0,0 +1,64 @@
+import { ChangeEventHandler, FC, InputHTMLAttributes } from "react";
+import * as Label from "@radix-ui/react-label";
+import { cn } from "@/lib/utils";
+
+export interface FieldProps extends InputHTMLAttributes<HTMLInputElement> {
+    id: string;
+    labelText: string;
+    inputText?: string;
+    InputElement?: React.ReactNode;
+}
+
+const Field: FC<FieldProps> = ({
+    labelText,
+    id,
+    InputElement,
+    className,
+    onChange,
+    type,
+    ...props
+}) => {
+    return (
+        <fieldset className="flex flex-col justify-center">
+            <Label.Root className="flex-1" htmlFor={id}>
+                {labelText}
+                <span className="text-red-500">
+                    {props.required ? "*" : null}
+                </span>
+            </Label.Root>
+            {InputElement ? (
+                InputElement
+            ) : type !== "textarea" ? (
+                <>
+                    <input
+                        className={cn(
+                            "flex-1 w-full outline-none bg-stone-100 text-stone-900 dark:bg-stone-900 px-5 py-2 rounded-md focus-within:ring ring-orange-500/20",
+                            className
+                        )}
+                        id={id}
+                        name={id}
+                        onChange={onChange}
+                        type={type}
+                        {...props}
+                    />
+                </>
+            ) : (
+                <textarea
+                    className={cn(
+                        "flex-1 w-full outline-none bg-stone-100 text-stone-900 dark:bg-stone-900 px-5 py-2 rounded-md focus-within:ring ring-orange-500/20",
+                        className
+                    )}
+                    name={id}
+                    rows={5}
+                    id={id}
+                    onChange={
+                        onChange as
+                            | ChangeEventHandler<HTMLTextAreaElement>
+                            | undefined
+                    }
+                ></textarea>
+            )}
+        </fieldset>
+    );
+};
+export default Field;
