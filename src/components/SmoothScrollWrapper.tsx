d77622e867ef86f0f0a98e8911d578f659fe8839
iMADi-ARCH <nandanaditya985@gmail.com>
2023-05-01 18:15:51 +0530

    feature: Happy accident added blobity

--- src/components/SmoothScrollWrapper.tsx
+++ src/components/SmoothScrollWrapper.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { FC } from "react";
 import { Lenis as ReactLenis, useLenis } from "@studio-freight/react-lenis";
+import useBlobity from "blobity/lib/react/useBlobity";
 interface SmoothScrollWrapperProps {
     children?: React.ReactNode;
 }
@@ -8,6 +9,14 @@ interface SmoothScrollWrapperProps {
 const SmoothScrollWrapper: FC<SmoothScrollWrapperProps> = ({ children }) => {
     const lenis = useLenis(({}) => {
         // called every scroll
+    });
+    const blobity = useBlobity({
+        licenseKey: "opensource",
+        invert: true,
+        dotColor: "#000000",
+        tooltipPadding: 10,
+        radius: 900,
+        opacity: 0.1,
     });
 
     return (
