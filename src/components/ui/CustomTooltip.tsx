f348ba555a9cbdad094b7322c01b7cb69d8eae40
iMADi-ARCH <nandanaditya985@gmail.com>
2023-05-03 14:29:02 +0530

    fix: Exit animation

--- src/components/ui/CustomTooltip.tsx
+++ src/components/ui/CustomTooltip.tsx
@@ -1,7 +1,7 @@
 "use client";
-import { FC } from "react";
+import { FC, useState } from "react";
 import * as Tooltip from "@radix-ui/react-tooltip";
-import { AnimatePresence, motion } from "framer-motion";
+import { AnimatePresence, motion, useScroll } from "framer-motion";
 
 interface CustomTooltipProps extends Tooltip.TooltipProps {
     tooltipText: string;
@@ -14,33 +14,37 @@ const CustomTooltip: FC<CustomTooltipProps> = ({
     side,
     ...props
 }) => {
+    const [open, setOpen] = useState(false);
+
     return (
         <Tooltip.Provider {...props}>
-            <Tooltip.Root>
+            <Tooltip.Root open={open} onOpenChange={setOpen}>
                 <Tooltip.Trigger className="text-left">
                     {children}
                 </Tooltip.Trigger>
-                <Tooltip.Portal>
-                    <AnimatePresence>
-                        <Tooltip.Content
-                            asChild
-                            className="select-none rounded-md bg-stone-50 px-3 py-2 text-base leading-none shadow-lg will-change-[transform,opacity] text-black"
-                            sideOffset={5}
-                            side={side || "top"}
-                            forceMount
-                        >
-                            <motion.div
-                                initial={{ opacity: 0, y: "-100%" }}
-                                animate={{ opacity: 1, y: "10%" }}
-                                exit={{ opacity: 0, y: "-100%" }}
+                <AnimatePresence>
+                    {open ? (
+                        <Tooltip.Portal forceMount>
+                            <Tooltip.Content
+                                asChild
+                                forceMount
+                                sideOffset={5}
+                                side={side || "top"}
                             >
-                                {tooltipText}
-                                <Tooltip.Arrow className="fill-white" />
-                            </motion.div>
-                            {/* {tooltipText} */}
-                        </Tooltip.Content>
-                    </AnimatePresence>
-                </Tooltip.Portal>
+                                <motion.div
+                                    className="select-none rounded-md bg-stone-100 px-3 py-2 text-base border border-current leading-none shadow-lg will-change-[transform,opacity] text-black"
+                                    initial={{ opacity: 0, y: "-10%" }}
+                                    animate={{ opacity: 1, y: "10%" }}
+                                    exit={{ opacity: 0, y: "-10%" }}
+                                >
+                                    {tooltipText}
+                                    {/* <Tooltip.Arrow className="fill-stone-100" /> */}
+                                </motion.div>
+                                {/* {tooltipText} */}
+                            </Tooltip.Content>
+                        </Tooltip.Portal>
+                    ) : null}
+                </AnimatePresence>
             </Tooltip.Root>
         </Tooltip.Provider>
     );
