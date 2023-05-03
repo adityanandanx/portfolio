305c8493545e2d4b09482f24470ebf3ba05713dd
iMADi-ARCH <nandanaditya985@gmail.com>
2023-05-03 13:20:19 +0530

    feature: Content added

---
+++ src/app/nav/Content.tsx
@@ -0,0 +1,95 @@
+import { Variants, motion } from "framer-motion";
+import { Syne } from "next/font/google";
+import Link from "next/link";
+import { FC, ReactNode } from "react";
+import { Url } from "url";
+
+const syne = Syne({ subsets: ["latin"], weight: ["700"] });
+interface Link {
+    name: string;
+    href: string | Url;
+}
+
+const LINKS: Link[] = [
+    { name: "Home", href: "/" },
+    { name: "About Me", href: "/about" },
+    { name: "Projects", href: "/projects" },
+    { name: "Testimonials", href: "/testimonials" },
+    { name: "Contact", href: "/contact" },
+];
+
+interface ContentProps {}
+
+const Content: FC<ContentProps> = ({}) => {
+    const containerVariant: Variants = {
+        hidden: {},
+        visible: {
+            transition: {
+                staggerChildren: 0.075,
+            },
+        },
+    };
+    return (
+        <motion.div
+            style={syne.style}
+            className="relative w-full h-full text-4xl sm:text-7xl md:text-8xl overflow-hidden"
+        >
+            <motion.ul
+                variants={containerVariant}
+                initial={"hidden"}
+                animate={"visible"}
+                className="flex flex-col h-full max-h-screen justify-center py-20"
+            >
+                {LINKS.map((l, i) => {
+                    return (
+                        <NavLink key={i} href={l.href}>
+                            {l.name}
+                        </NavLink>
+                    );
+                })}
+            </motion.ul>
+        </motion.div>
+    );
+};
+
+const AnimatedLink = motion(Link);
+
+const NavLink: FC<{ children?: React.ReactNode; href: string | Url }> = ({
+    children,
+    href,
+}) => {
+    const itemVariant: Variants = {
+        hidden: {
+            // opacity: -1,
+            y: "100%",
+        },
+        visible: {
+            // opacity: 1,
+            y: "0%",
+            // transition: {},
+        },
+    };
+
+    return (
+        <li className="overflow-hidden">
+            <AnimatedLink
+                variants={itemVariant}
+                whileTap={{ scale: 0.95 }}
+                whileHover={{
+                    scale: 1.05,
+                    color: "rgb(250 250 249)",
+                    transition: {
+                        duration: 0.24,
+                    },
+                }}
+                className="block p-5 text-stone-400 origin-left"
+                href={"/"}
+            >
+                {/* <span className="text-stone-500 text-8xl leading-10">/</span> */}
+                {children}
+            </AnimatedLink>
+        </li>
+    );
+};
+
+export default Content;
--- src/app/nav/index.tsx
+++ src/app/nav/index.tsx
@@ -6,7 +6,7 @@ import Button from "@/components/ui/Button";
 import { Syne } from "next/font/google";
 import { AnimatePresence, MotionConfig, motion } from "framer-motion";
 import LetterAnim from "@/components/animations/LetterAnim";
-// import Content from "./Content";
+import Content from "./Content";
 
 const syne = Syne({ subsets: ["latin"], weight: ["700"] });
 
@@ -20,7 +20,7 @@ const Nav: FC<navProps> = ({}) => {
                 <motion.button
                     animate={{ skewX: open ? "-30deg" : "0deg" }}
                     whileTap={{ scale: 0.95 }}
-                    className="text-5xl fixed top-0 right-0 z-20 cursor-pointer p-10 text-stone-50 mix-blend-difference"
+                    className="text-5xl fixed top-0 right-0 z-20 cursor-pointer p-10 text-stone-50 mix-blend-difference outline-none"
                 >
                     <MdMenu className="" />
                 </motion.button>
@@ -36,24 +36,23 @@ const Nav: FC<navProps> = ({}) => {
                                 className="fixed inset-0 bg-stone-900/50"
                             ></motion.div>
                         </Dialog.Overlay>
-                        {/* <AnimatePresence> */}
                         <Dialog.Content asChild forceMount>
                             <motion.div
                                 initial={{ x: "-100%" }}
                                 animate={{ x: "0%" }}
                                 exit={{ x: "-100%" }}
                                 transition={{ ease: "circOut" }}
-                                className="fixed z-30 inset-0 bg-stone-900 text-stone-50 p-[25px] shadow-lg focus:outline-none"
+                                className="fixed z-30 inset-0 bg-stone-900 text-stone-50 p-5 flex flex-col shadow-lg focus:outline-none"
                             >
                                 <MotionConfig
                                     transition={{
-                                        duration: 0.6,
+                                        duration: 1,
                                         ease: "anticipate",
                                     }}
                                 >
-                                    <Dialog.Title
+                                    {/* <Dialog.Title
                                         style={syne.style}
-                                        className="text-8xl leading-none overflow-hidden"
+                                        className="text-5xl p-10 leading-none overflow-hidden opacity-10 absolute"
                                     >
                                         <motion.h1
                                             initial={{ y: "100%" }}
@@ -63,13 +62,13 @@ const Nav: FC<navProps> = ({}) => {
                                         >
                                             Aditya Nandan
                                         </motion.h1>
-                                    </Dialog.Title>
-                                    <hr className="w-full border border-current" />
-                                    {/* <Content /> */}
+                                    </Dialog.Title> */}
+                                    {/* <hr className="w-full border border-current opacity-10" /> */}
+                                    <Content />
                                     <Dialog.Close asChild>
                                         <motion.button
                                             whileTap={{ scale: 0.95 }}
-                                            className="absolute top-0 right-0 p-10 text-5xl"
+                                            className="absolute top-0 right-0 p-10 text-5xl outline-none"
                                             aria-label="Close"
                                         >
                                             <MdClose />
@@ -78,7 +77,6 @@ const Nav: FC<navProps> = ({}) => {
                                 </MotionConfig>
                             </motion.div>
                         </Dialog.Content>
-                        {/* </AnimatePresence> */}
                     </Dialog.Portal>
                 ) : null}
             </AnimatePresence>
