12976775f66e1afcb4cd2de80cff678cb42db53d
iMADi-ARCH <nandanaditya985@gmail.com>
2023-08-31 09:57:37 +0530

    feat: add socials to hero

--- src/app/hero/index.tsx
+++ src/app/hero/index.tsx
@@ -6,71 +6,87 @@ import CustomTooltip from "@/components/ui/CustomTooltip";
 import { motion, useScroll, useTransform } from "framer-motion";
 import Heading from "@/components/ui/Heading";
 import { MdChevronRight } from "react-icons/md";
+import Socials from "@/components/ui/Socials";
+import { FaLinkedin, FaTwitter, FaGithub } from "react-icons/fa";
 
 interface HeroProps {}
 
 const Hero: FC<HeroProps> = ({}) => {
-    const target = useRef<HTMLDivElement>(null);
-    const { scrollYProgress } = useScroll({
-        target: target,
-        offset: ["start end", "end start"],
-    });
-    const opacity = useTransform(scrollYProgress, [0.5, 1], [1, 0]);
-    const scale = useTransform(scrollYProgress, [0.5, 1], ["100%", "90%"]);
-    const y = useTransform(scrollYProgress, [0.5, 1], ["0%", "-100%"]);
-    const position = useTransform(scrollYProgress, (pos) => {
-        return pos >= 1 ? "relative" : "fixed";
-    });
+  const target = useRef<HTMLDivElement>(null);
+  const { scrollYProgress } = useScroll({
+    target: target,
+    offset: ["start end", "end start"],
+  });
+  const opacity = useTransform(scrollYProgress, [0.5, 1], [1, 0]);
+  const scale = useTransform(scrollYProgress, [0.5, 1], ["100%", "90%"]);
+  const y = useTransform(scrollYProgress, [0.5, 1], ["0%", "-100%"]);
+  const position = useTransform(scrollYProgress, (pos) => {
+    return pos >= 1 ? "relative" : "fixed";
+  });
 
-    return (
-        <motion.section
-            id="home"
-            ref={target}
-            style={{ opacity }}
-            className="relative w-full h-screen mb-[100vh]"
-        >
-            <motion.div
-                style={{ position }}
-                className="fixed inset-0 top-0 p-10 flex flex-col justify-between h-full"
+  return (
+    <motion.section
+      id="home"
+      ref={target}
+      style={{ opacity }}
+      className="relative w-full h-screen mb-[100vh]"
+    >
+      <motion.div
+        style={{ position }}
+        className="fixed inset-0 top-0 p-10 flex flex-col justify-between h-full"
+      >
+        <Heading className="w-fit max-w-[70vw]" style={{ scale }}>
+          Hello There! <br />I am <br />
+          <CustomTooltip side="bottom" delayDuration={0} tooltipText="Say Hi!">
+            <Link
+              href={"/#contact"}
+              className="inline-block w-fit text-left outline-none text-orange-600 dark:text-orange-400 px-3 py-0 -mx-3 bg-stone-100 dark:bg-stone-800 leading-none rounded-lg"
             >
-                <Heading className="w-fit max-w-[70vw]" style={{ scale }}>
-                    Hello There! <br />I am <br />
-                    <CustomTooltip
-                        side="bottom"
-                        delayDuration={0}
-                        tooltipText="Say Hi!"
-                    >
-                        <Link
-                            href={"/#contact"}
-                            className="inline-block w-fit text-left outline-none text-orange-600 dark:text-orange-400 px-3 py-0 -mx-3 bg-stone-100 dark:bg-stone-800 leading-none rounded-lg"
-                        >
-                            Aditya Nandan
-                        </Link>
-                    </CustomTooltip>
-                </Heading>
-                <motion.div
-                    style={{ y }}
-                    className="max-w-lg w-[50vw] text-base flex flex-col gap-10"
-                >
-                    <p>
-                        I am a self-taught Frontend Web Developer and Designer
-                        and I make unique web experiences for small to large
-                        scale brands.
-                    </p>
-                    <div className="flex flex-col items-start gap-3">
-                        <Link href={"/#contact"}>
-                            <Button>
-                                Get In Touch <MdChevronRight />
-                            </Button>
-                        </Link>
-                        <Link href={"/#projects"}>
-                            <Button variant={"secondary"}>Projects</Button>
-                        </Link>
-                    </div>
-                </motion.div>
-            </motion.div>
-        </motion.section>
-    );
+              Aditya Nandan
+            </Link>
+          </CustomTooltip>
+        </Heading>
+        <motion.div style={{ y }} className="text-base flex flex-col gap-10">
+          <p className="max-w-lg w-[50vw] ">
+            I am a self-taught Frontend Web Developer and Designer and I make
+            unique web experiences for small to large scale brands.
+          </p>
+          <div className="flex justify-between items-end w-full">
+            <div className="flex flex-col items-start gap-3">
+              <Link href={"/#contact"}>
+                <Button>
+                  Get In Touch <MdChevronRight />
+                </Button>
+              </Link>
+              <Link href={"/#projects"}>
+                <Button variant={"secondary"}>Projects</Button>
+              </Link>
+            </div>
+            <Socials
+              className="text-stone-50"
+              socials={[
+                {
+                  name: "Linkedin",
+                  href: "https://www.linkedin.com/in/aditya-nandan-thats-it/",
+                  icon: <FaLinkedin />,
+                },
+                {
+                  name: "Twitter",
+                  href: "https://twitter.com/iMADi69235681",
+                  icon: <FaTwitter />,
+                },
+                {
+                  name: "Github",
+                  href: "https://github.com/iMADi-ARCH/cryptracker",
+                  icon: <FaGithub />,
+                },
+              ]}
+            />
+          </div>
+        </motion.div>
+      </motion.div>
+    </motion.section>
+  );
 };
 
 export default Hero;
--- src/app/talk/index.tsx
+++ src/app/talk/index.tsx
@@ -10,67 +10,67 @@ import LetterAnim from "../../components/animations/LetterAnim";
 interface TalkProps {}
 
 const Talk: FC<TalkProps> = ({}) => {
-    return (
-        <section id="contact" className="relative w-full p-0">
-            <div className="w-full h-full p-10 py-16 md:p-24 bg-stone-800 dark:bg-stone-700 text-stone-50 rounded-t-xl flex flex-col items-start justify-between">
-                <div className="h-full">
-                    <h2 className="text-2xl font-Syne font-medium">
-                        <LetterAnim text="Have a project for Me?" />
-                    </h2>
-                    <h1 className="text-7xl sm:text-8xl md:text-9xl font-Syne font-bold">
-                        <LetterAnim text="Let's Talk" />
-                        {/* Let&rsquo;s Talk */}
-                    </h1>
-                    <form
-                        action="mailto:dev2005adi@gmail.com"
-                        method="get"
-                        encType="text/plain"
-                        className="max-w-md flex flex-col gap-3 flex-1 my-20"
-                    >
-                        <Field required id="subject" labelText="Subject" />
-                        <Field required id="email" labelText="Your Email" />
-                        <Field
-                            required
-                            id="body"
-                            labelText="Message"
-                            placeholder="Your Message"
-                            type="textarea"
-                        />
-                        <Button className="text-base leading-none py-4 bg-stone-50 text-stone-900 self-start">
-                            Get in touch
-                            <MdChevronRight className="transform scale-150" />
-                        </Button>
-                    </form>
-                </div>
-                <footer className="w-full flex flex-col md:flex-row md:items-center justify-between">
-                    <span className="text-2xl font-Syne font-medium">
-                        <LetterAnim text="Aditya Nandan" />
-                        {/* Aditya Nandan */}
-                    </span>
-                    <Socials
-                        className="text-stone-50"
-                        socials={[
-                            {
-                                name: "Linkedin",
-                                href: "https://www.linkedin.com/in/aditya-nandan-thats-it/",
-                                icon: <FaLinkedin />,
-                            },
-                            {
-                                name: "Twitter",
-                                href: "https://twitter.com/iMADi69235681",
-                                icon: <FaTwitter />,
-                            },
-                            {
-                                name: "Github",
-                                href: "https://github.com/iMADi-ARCH/cryptracker",
-                                icon: <FaGithub />,
-                            },
-                        ]}
-                    />
-                </footer>
-            </div>
-        </section>
-    );
+  return (
+    <section id="contact" className="relative w-full p-0">
+      <div className="w-full h-full p-10 py-16 md:p-24 bg-stone-800 dark:bg-stone-700 text-stone-50 rounded-t-xl flex flex-col items-start justify-between">
+        <div className="h-full">
+          <h2 className="text-2xl font-Syne font-medium">
+            <LetterAnim text="Have a project for Me?" />
+          </h2>
+          <h1 className="text-7xl sm:text-8xl md:text-9xl font-Syne font-bold">
+            <LetterAnim text="Let's Talk" />
+            {/* Let&rsquo;s Talk */}
+          </h1>
+          <form
+            action="mailto:dev2005adi@gmail.com"
+            method="get"
+            encType="text/plain"
+            className="max-w-md flex flex-col gap-3 flex-1 my-20"
+          >
+            <Field required id="subject" labelText="Subject" />
+            <Field required id="email" labelText="Your Email" />
+            <Field
+              required
+              id="body"
+              labelText="Message"
+              placeholder="Your Message"
+              type="textarea"
+            />
+            <Button className="text-base leading-none py-4 bg-stone-50 text-stone-900 self-start">
+              Get in touch
+              <MdChevronRight className="transform scale-150" />
+            </Button>
+          </form>
+        </div>
+        <footer className="w-full flex flex-col md:flex-row md:items-center justify-between">
+          <span className="text-2xl font-Syne font-medium">
+            <LetterAnim text="Aditya Nandan" />
+            {/* Aditya Nandan */}
+          </span>
+          <Socials
+            className="text-stone-50"
+            socials={[
+              {
+                name: "Linkedin",
+                href: "https://www.linkedin.com/in/aditya-nandan-thats-it/",
+                icon: <FaLinkedin />,
+              },
+              {
+                name: "Twitter",
+                href: "https://twitter.com/iMADi69235681",
+                icon: <FaTwitter />,
+              },
+              {
+                name: "Github",
+                href: "https://github.com/iMADi-ARCH/cryptracker",
+                icon: <FaGithub />,
+              },
+            ]}
+          />
+        </footer>
+      </div>
+    </section>
+  );
 };
 
 export default Talk;
